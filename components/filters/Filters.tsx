0af81b05e7e251d9cecb6ffec883198b5debadd3
gekoJs <daniroa.js@gmail.com>
2023-07-03 23:19:56 -0400

    fix two

--- components/filters/Filters.tsx
+++ components/filters/Filters.tsx
@@ -124,12 +124,13 @@ export default function Filters({
     setOptionsSearched(data);
   };
 
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState<number>(0);
   useEffect(() => {
     const handleWindowResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleWindowResize);
     return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
+  
   return (
     <div className={style.container}>
       {/* //TITLE ----------------------------------- */}
